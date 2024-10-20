773bdebf5c4cc7af1a1b981ffad3fd2cfeab6bec
Newriver758 <shinkawa0109@gmail.com>
2024-10-20 17:39:39 +0900

    active hushの導入, アソシエーションの設定等

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "donations#index"
+  resources :donations, only: [:index, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
